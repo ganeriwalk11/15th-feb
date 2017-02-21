42a692c271bf64257f112f3e2e1a0962eaf89deb
Krishna Ganeriwal <ganeriwalk@gmail.com>
2017-02-21 08:51:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
-import axios from 'axios';
 import Rx from 'rxjs';
 import 'rxjs/add/observable/dom/ajax';
 import { Observable } from 'rxjs/Observable';
+import axios from 'axios';
 
+export const POST_DATA = 'POST_DATA';
 export const FETCH_DATA = 'FETCH_DATA';
 export const FETCH_FUL = 'FETCH_USER_FULFILLED';
 export const FETCH_FULL = 'FETCH_USER_F';
-export const FETCH_STOCK = 'FETCH_STOCK';
 export const ADD_DATA = 'ADD_DATA';
 export const ADD_COL = 'ADD_COL';
 export const CHECK_INTEGER = 'CHECK_INTEGER';
-export const APPLY_FORMULA = 'APPLY_FORMULA';
 export const APPLY_FUN = 'APPLY_FUN';
+export const APPLY_FUNCTION = 'APPLY_FUNCTION';
+export const S_COLOR = 'S_COLOR';
 
 const url = 'src/jsonData/mainData.json';
-
+const urla = 'http://localhost:5000';
 export const fetchUserEpic = action$ =>
   action$.ofType(FETCH_DATA)
     .mergeMap(action =>
       Observable.ajax.getJSON(url)
         .map(response => fetchUserFulfilled(response))
-        
     );
 
-export const fetchUserFulfilled = payload => ({ type: FETCH_FUL, payload });
+export const fetchUserFulfilled = payload => (
+{ 
+  type: FETCH_FUL,
+  payload 
+});
 
-export function fetchData() {
-  return {
+export function fetchData()
+{
+  return{
     type: FETCH_DATA
   }
 };
@@ -40,56 +45,50 @@ export function fetchUrlData(data) {
 
  
 export function postData(data) {
- // console.log(data);
-  //const url = 'src/jsonData/mainData.json';
-  const request = axios.post(url,data);
-  return ;
-  //    return {
-  //   type: FETCH_DATA,
-  //   payload: request
-  // }
+  const request = axios.post(urla,data);
+     return {
+    type: POST_DATA
+  }
 };
 
-export function addData(add) {
- var addRow = add;
+export function addData(dupdata) {
  return {
     type: ADD_DATA,
-    payload: addRow
+    payload: dupdata
   };
 };
 
-export function addColData(add) {
- var addColumn = add;
- return {
+export function addColData(add)
+{
+  var addColumn = add;
+  return{
     type: ADD_COL,
     payload: addColumn
   };
 };
 
-export function checkIntegerAction(i,j,h,data,a) 
+export function checkIntegerAction(i,h,target) 
 {
-  var a = a;
-  var data = data;
-  var color;     
-  if(a == parseInt(a, 10))
-  {
-    color = 'green';
-    var head  = h["h"];
-  }
-  else
-  {
-    color = 'red';
-  }  
   return {
     type: CHECK_INTEGER,
     payload: {
       i:i,
-      j:j,
       h:h,
-      color: color,
-      target:a
+      target: target
     }
-  }
+  };
+};
+
+export function stringColor(h,i,target) 
+{
+  return {
+    type: S_COLOR,
+    payload: {
+      i:i,
+      h:h,
+      target:target
+    }
+  };
 };
 
 // export const applyFunctionEpic = action$ =>
@@ -132,13 +131,13 @@ export function checkIntegerAction(i,j,h,data,a)
 //       // .subscribe(function(data){applyFunc(data);});
       
 
-export function applyFunc(response) 
-{
-  return {
-    type: APPLY_FUNC,
-    payload : response
-  }
-};
+// export function applyFunc(response) 
+// {
+//   return {
+//     type: APPLY_FUNC,
+//     payload : response
+//   }
+// };
 
 
 export function applyF(h,a,i,data,color) 
@@ -148,24 +147,25 @@ export function applyF(h,a,i,data,color)
   var header = h;
   var color = color;
   let alpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-        var i = i;
-        var head = Object.keys(data[0]);
-        var len = head.length;
-        var op1i = a[3] - 1;
-        var op2i = a[6] - 1;
-        var op1j = alpha.indexOf(a[2]);
-        var op2j = alpha.indexOf(a[5]);
-        var operator = a[4];
-        var ans;
-
-        if(operator == '+'){
-          ans = parseInt(data[op1i][head[op1j]][head[op1j]],10) + parseInt(data[op2i][head[op2j]][head[op2j]],10);
-        }
-        else{
-          ans = parseInt(data[op1i][head[op1j]][head[op1j]],10) - parseInt(data[op2i][head[op2j]][head[op2j]],10);
+  var i = i;
+  var head = Object.keys(data[0]);
+  var len = head.length;
+  var op1i = a[3] - 1;
+  var op2i = a[6] - 1;
+  var op1j = alpha.indexOf(a[2]);
+  var op2j = alpha.indexOf(a[5]);
+  var operator = a[4];
+  var ans;
+  if(operator == '+')
+  {
+    ans = parseInt(data[op1i][head[op1j]]['value'],10) + parseInt(data[op2i][head[op2j]]['value'],10);
+  }
+  else
+  {
+    ans = parseInt(data[op1i][head[op1j]]['value'],10) - parseInt(data[op2i][head[op2j]]['value'],10);
          // console.log((data[op1i][head[op1j]][head[op1j]],10),parseInt(data[op2i][head[op2j]][head[op2j]],10),ans);
-        }
-        var response = {
+  }
+  var response = {
           op1i: op1i,
           op1j:op1j,
           op2i: op2i,
@@ -177,7 +177,60 @@ export function applyF(h,a,i,data,color)
           a:a,
           color:color,
           header: header
-        };
-       // console.log("response->",response);
-        return {type:APPLY_FUN,payload:response};
+    };
+    return {type:APPLY_FUN,payload:response};
+};
+
+
+export function applyFunc(h,a,i,data,color,op1,op2,op1i,op1j,op2i,op2j,operator) 
+{
+  var a = a;
+  var data = data;
+  var header = h;
+  var color = color;
+  let alpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  var i = i;
+  var head = Object.keys(data[0]);
+  var len = head.length;
+  var op1i = op1i;
+  var op1j = op1j;
+  var op2i = op2i;
+  var op2j = op2j;
+  var operator = operator;
+  var op1 = op1;
+  var op2 = op2;
+  var ans;
+  if(op1 !== "")
+  {
+    if(operator == '+')
+      ans = op1 + parseInt(data[op2i][head[op2j]]['value'],10);
+    else
+      ans = op1 - parseInt(data[op2i][head[op2j]]['value'],10);
+  }
+  else if(op2 !== "")
+  {
+    if(operator == '+')
+      ans = op2 + parseInt(data[op1i][head[op1j]]['value'],10);
+    else
+      ans = op2 - parseInt(data[op1i][head[op1j]]['value'],10);
+  }
+  else if(op2i === "" )
+  {
+      ans = parseInt(data[op1i][head[op1j]]['value'],10)
+  }
+  
+  var response = {
+          op1i: op1i,
+          op1j:op1j,
+          op2i: op2i,
+          op2j:op2j,
+          ans: ans,
+          data:data,
+          head,head,
+          i:i,
+          a:a,
+          color:"blue",
+          header: header
+    };
+    return {type:APPLY_FUNCTION,payload:response};
 };
