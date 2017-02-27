c63eff338205e3552a2fb06d52cf24b4bfc5df8b
Krishna Ganeriwal <ganeriwalk@gmail.com>
2017-02-27 08:45:45 +0530

    Update checkIDReducer.js

--- checkIDReducer.js
+++ checkIDReducer.js
@@ -1,43 +1,21 @@
 import { fx_bar } from '../actions/validations';
 import { f_bar } from '../actions/validations';
-import { APPLY_F } from '../actions/validations';
 
-export default function(state ="", action) {
-  switch (action.type) {
+export default function (state = "", action) {
+    switch (action.type) {
 
-    case APPLY_F: {
-        var alpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-        var s = action.payload.fx;
-        var l = action.payload.l;
-        if(s[0] != '=' ){
-            alert("Not valid, missing =");
-        }
-        else
-        {
-            if(alpha.indexOf(s[1]) > l-1 || alpha.indexOf(s[3]) > l-1){
-                alert("Invalid operands for calculations");
+        case fx_bar:
+            return action.payload;
+            break;
+
+        case f_bar:
+            {
+                return action.payload;
+                break;
             }
-            else{
-                if(s[1]=='+'){
-                    
-                }
-            }
-        }
-        break;
+
+            defualt: return state;
     }
-
-    case fx_bar:
-        return action.payload;
-        break;
-
-    case f_bar:{
-        return action.payload;
-        break;
-    }
-
-    defualt: return state;
-    
-  }
-  return state;
+    return state;
 }
 
